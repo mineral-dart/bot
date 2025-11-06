d92a8795351a343620c488199c10ae1773ee3bd6
pandaguerrier <juleslofficial@gmail.com>
2025-11-06 13:29:37 +0100

    feat: use baptiste mega cool func :p

--- lib/application/common/common_provider.dart
+++ lib/application/common/common_provider.dart
@@ -2,12 +2,12 @@ import 'dart:io';
 
 import 'package:mineral/api.dart';
 import 'package:mineral/container.dart';
+import 'package:mineral/utils.dart';
 import 'package:mineralbot/application/common/entities/voice_config.dart';
 import 'package:mineralbot/application/common/events/voice_join.dart';
 import 'package:mineralbot/application/common/events/voice_leave.dart';
 import 'package:mineralbot/application/common/events/welcome_event.dart';
 import 'package:mineralbot/application/common/states/voice_demand_state.dart';
-import 'package:yaml/yaml.dart';
 
 final class CommonProvider extends Provider with State {
   final Client _client;
@@ -22,9 +22,8 @@ final class CommonProvider extends Provider with State {
 
   @override
   void ready() {
-    final configFile = File("config/common.yml");
-    final payload = loadYaml(configFile.readAsStringSync());
-    ioc.make<CommonConfig>(() => CommonConfig.fromYml(payload));
+    final config = File("config/common.yml").readAsYamlSync();
+    ioc.make<CommonConfig>(() => CommonConfig.of(config));
 
     print("Common provider is ready !");
   }
